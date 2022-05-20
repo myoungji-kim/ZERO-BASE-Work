7d349ee8467116f67fd367a9c3ea2deffb5b0768
Myoung Ji Kim <myoung9412@daum.net>
2022-05-20 12:07:57 +0900

    Create Practice2-1.java

---
+++ Practice/Java01/Practice2-1.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+// 로마 숫자 표기 -> 정수형으로 변환
+
+public class Practice1 {
+    public static void solution(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = 0;
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (map.get(arr[i]) < map.get(arr[i + 1])) {
+                sum -= map.get(arr[i]);
+            } else {
+                sum += map.get(arr[i]);
+            }
+        }
+        sum += map.get(arr[arr.length - 1]);
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        solution("III");
+        solution("IV");
+        solution("VI");
+        solution("XIII");
+        solution("XXVI");
+        solution("MCMXCIV");
+    }
+}
