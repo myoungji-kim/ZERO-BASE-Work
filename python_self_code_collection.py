04ec7daac66d88578ca001d10997648b0e4c85bf
Myoung Ji Kim <myoung9412@daum.net>
2022-06-03 11:31:00 +0900

    Update python_self_code_collection.py

--- python_self_code_collection.py
+++ python_self_code_collection.py
@@ -86,6 +86,17 @@ print(lcm(21, 14)) # 최소 공배수 (LCM) 계산 -> 42
 
 
 
+# 유클리드 호제법 (최대공약수 계산)
+def gcd(a, b):
+  if a % b == 0:
+    return b
+  else:
+    return gcd(b, a % b)
+
+print(gcd(192, 162))
+
+
+
 # 방향 벡터 (간단ver)
 n = 5
 x, y = 1, 1
