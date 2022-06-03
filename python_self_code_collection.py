d52cdd753a2adf562b3d7a3c922974564857f324
Myoung Ji Kim <myoung9412@daum.net>
2022-06-03 10:50:08 +0900

    Update python_self_code_collection.py

--- python_self_code_collection.py
+++ python_self_code_collection.py
@@ -221,3 +221,102 @@ result = heapsort([1, 3, 5, 9, 2, 4, 6, 8, 0])
 print(result) ## [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
 
+
+# 다익스트라 베이직
+import heapq
+from sys import stdin
+
+INF = int(1e9)  # 무한, 10억
+
+# 노트의 개수, 간선의 개수를 입력 받기
+n, m = map(int, stdin.readline().split())
+
+# 시작 노드 번호를 입력받기
+start = int(stdin.readline())
+
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    a, b, c = map(int, stdin.readline().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 거리는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q: # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+# 다익스트라 알고리즘을 수행
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INF)라고 출력
+    if distance[i] == INF:
+        print("INF")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(distance[i])
+
+
+
+# 플로이드 워셜 베이직 (다익스트라)
+from sys import stdin
+
+INF = int(1e9)
+
+n, m = map(int, stdin.readline().split())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int, stdin.readline().split())
+    graph[a][b], graph[b][a] = 1, 1
+
+x, k = map(int, stdin.readline().split())
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print("-1")
+else:
+    print(distance)
+
+    
+    
+
+    
+    
+    
+    
+    
